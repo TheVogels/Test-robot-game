aa160afbee2d4df4c497245d63df2ffbb0e9327e
TheVogels <85116034+TheVogels@users.noreply.github.com>
2023-03-18 20:07:04 -0500

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,98 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "Sz-|~E(8*Z":
+            case "Charger":return img`
+. . d d . . . . . . . . . . 
+. 1 1 1 d 1 1 1 1 1 1 1 1 . 
+1 f f f f f f f f f f f f 1 
+1 f 1 1 1 1 1 1 1 1 1 1 f 1 
+1 f 1 1 1 1 1 1 1 1 1 1 f 1 
+1 f 1 1 5 5 1 1 5 5 1 1 f 1 
+1 f 1 1 5 5 1 1 5 5 1 1 f 1 
+1 f 1 1 5 5 1 1 5 5 1 1 f 1 
+1 f 1 1 5 5 1 1 5 5 1 1 f 1 
+1 f 1 1 1 1 1 1 1 1 1 1 f 1 
+1 f 1 1 1 1 1 1 1 1 1 1 f 1 
+1 f 1 1 1 1 1 1 1 1 1 1 f 1 
+. 1 1 1 1 1 1 1 1 1 1 1 1 . 
+`;
+            case "_o*ElMMb9Txj7T}L$)_j":
+            case "Player":return img`
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 f f f f f f f f f f f 
+1 1 1 1 1 f f f 9 f f f f 9 f f 
+1 1 1 1 1 f f 9 9 9 f f 9 9 9 f 
+1 1 1 1 1 f f f 9 f f f f 9 f f 
+1 1 1 1 1 f f f f f f f f f f f 
+1 1 1 1 1 f f f f f f f f f f f 
+1 1 1 1 1 f f f f f f f f f f f 
+1 1 1 1 1 f f f f f f f f f f f 
+1 1 1 1 1 f f f f f f f f f f f 
+1 1 1 1 1 f f f f f 9 f f f f f 
+1 1 1 1 1 f f f f 9 9 9 f f f f 
+1 1 1 1 1 f f f f f 9 f f f f f 
+1 1 1 1 1 f f f f f f f f f f f 
+1 1 1 1 1 f f f f f f f f f f f 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+`;
+            case "image2":
+            case "Player2":return img`
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+f f f f f f f f f f f 1 1 1 1 1 
+f f 9 f f f f 9 f f f 1 1 1 1 1 
+f 9 9 9 f f 9 9 9 f f 1 1 1 1 1 
+f f 9 f f f f 9 f f f 1 1 1 1 1 
+f f f f f f f f f f f 1 1 1 1 1 
+f f f f f f f f f f f 1 1 1 1 1 
+f f f f f f f f f f f 1 1 1 1 1 
+f f f f f f f f f f f 1 1 1 1 1 
+f f f f f f f f f f f 1 1 1 1 1 
+f f f f f 9 f f f f f 1 1 1 1 1 
+f f f f 9 9 9 f f f f 1 1 1 1 1 
+f f f f f 9 f f f f f 1 1 1 1 1 
+f f f f f f f f f f f 1 1 1 1 1 
+f f f f f f f f f f f 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+`;
+            case "image1":
+            case "myImage":return img`
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+3 3 a a a a a a a a a a a a 3 3 
+3 3 a a a . . . . . . a a a 3 3 
+3 3 a a a . . . . . . a a a 3 3 
+3 3 a a a . . . . . . a a a 3 3 
+3 3 a a a . . . . . . a a a 3 3 
+3 3 a a a . . . . . . a a a 3 3 
+3 3 a a a a a a a a a a a a 3 3 
+3 3 a a a a a a a a a 5 a a 3 3 
+3 3 a a a a a a a a 5 5 5 a 3 3 
+3 3 a a a a a a a a a 5 a a 3 3 
+3 3 a a a a a a a a a a a a 3 3 
+3 3 a a a a a a a a a a a a 3 3 
+3 3 a a a a a a a a a a a a 3 3 
+3 3 a a a a a a a a a a a a 3 3 
+3 3 a a a a a a a a a a a a 3 3 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
